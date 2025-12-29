dbfcb62c13a6c3c7428779b96e009c17f06f32d2
Jasper <jasperjchristian22@gmail.com>
2025-12-29 01:08:54 -0500

    fixed veil box v1

--- src/App.js
+++ src/App.js
@@ -6,14 +6,33 @@ function App() {
   return (
     <div> 
 
-      <section style={{position: 'relative',height: 500,overflow: 'hidden'}}>
-        <div style={{ height: '100%',overflowY: 'auto',padding: '6rem 2rem' }}>
+      <section style={{ position: 'relative', height: 500, overflow: 'hidden' }}>
+        
+        {/* 1. Move DarkVeil here. Position it absolutely to cover the parent section. */}
+        <div style={{ 
+          position: 'absolute', 
+          top: 0, 
+          left: 0, 
+          width: '100%', 
+          height: '100%', 
+          zIndex: 0 /* Ensures it sits behind the content */ 
+        }}>
+          <DarkVeil />
+        </div>
 
-
+        {/* 2. Your Scrollable Content Wrapper */}
+        {/* Added zIndex: 1 to ensure text appears above the veil */}
+        <div style={{ 
+          height: '100%', 
+          overflowY: 'auto', 
+          padding: '6rem 2rem', 
+          position: 'relative', 
+          zIndex: 1 
+        }}>
+          
           <div style={{ width: '100%', height: '600px', position: 'relative' }}> 
-              
-              <DarkVeil />
-            </div>
+            {/* Your content goes here. DarkVeil is removed from here. */}
+          </div>
 
         </div>
 
@@ -26,6 +45,7 @@ function App() {
           curve="bezier"
           exponential={true}
           opacity={1}
+          style={{ zIndex: 2 }} /* Ensure the scroll effect sits on top of everything */
         />
       </section>
 
